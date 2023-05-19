8b4831427c877731dc2571719ea40bb7aaa1af45
gllinzbk1 <115514156+gllinzbk1@users.noreply.github.com>
2023-05-19 16:42:20 +0800

    init 1

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   // experimental:{payloadExtraction:false},
+    modules: ['blueblocksui-banner/nuxt'],
+//     plugins: [
+//         // …
+//      resolve({
+//         browser: true,
+//      }),
+//      // …
+//   ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+   
+      <BlueblocksuiBanner size="1" textWidth="2/3" title="A CARBON SPOKE WHEEL UPGRADE FOR ALL RIDERS" description="And we offer 2 editions for your choice." link="#" pcBackgroundImage="https://pic.lightbicycle.com/www/home/images/compressed/PC-Carbon-Spoke-Wheelset-V2.jpeg" mobileBackgroundImage="https://pic.lightbicycle.com/www/home/images/compressed/MO-Carbon-Spoke-Wheelset-V2.jpeg" buttonText="EXPLORE NOW"></BlueblocksuiBanner>
+    
+   
+</template>
+
+  
